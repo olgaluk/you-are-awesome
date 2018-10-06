578463a529fd29282363eace5c4f68da7cb9c707
Olga Luk <olga_ivanova_86@mail.ru>
2018-10-06 10:22:51 +0300

    guess function

--- src/index.js
+++ src/index.js
@@ -1,18 +1,44 @@
-// DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+let currentCount = 0;
+let currentCount1 = 1;
+const createEnumerableProperty = (a) => {return a;};
+const createNotEnumerableProperty = () => {return Symbol();} ;
+const createProtoMagicObject = () => {return Function;};
 
-// return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+const incrementor = () => {
+    currentCount++;
+    incrementor.toString=()=>{return currentCount};
+    return incrementor;};
+
+const asyncIncrementor = () => {      
+  let count = function() {return currentCount1++;};  
+  return count();};
+
+const createIncrementer = () => {    
+    function* foo() {    
+        let index = 1;    
+        while(index<10)
+          yield index++;
+    }
+    return foo()};
+
+const returnBackInSecond = (result) => {
+    return new Promise(function(resolve,reject) {
+    setTimeout(() => {resolve(result);
+      }, 1000);})};
+
+const getDeepPropertiesCount = (obj) => {
+   const arr = Object.keys(obj);
+   let count = arr.length;
+   for(let key of arr) {    
+       count+=getDeepPropertiesCount(obj[key]);
+     }
+    return count;
+};
+
+const createSerializedObject = () => {return new String;};
 const toBuffer = () => {};
-const sortByProto = () => {};
+const sortByProto = (arr) => {return arr.sort()};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
